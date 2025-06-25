1b29ead1a7e8649758893c0e4488674aa09fe078
unknown <vesarthai123@gmail.com>
2025-06-25 11:31:10 +0700

    print

--- lecture1/testprint.py
+++ lecture1/testprint.py
@@ -1,1 +1,8 @@
+from math import pi
+
 print("Watchakorn")
+print('''a\t\ta\n\ta\na\t\ta''')
+print('%d' %100.334)
+print('%.2f' %100.337)
+print('%.4f' %pi)
+print('my age is %d i have money %d' % (22, 1000))
